9a5daca39ca1fb595c35990ec5f96fc24c20b999
qingshuidousha <827924175@qq.com>
2021-11-27 16:53:11 +0800

    1

--- train-ticket/src/query/App.jsx
+++ train-ticket/src/query/App.jsx
@@ -41,11 +41,112 @@ import {
 import "./App.css";
 
 function App(props) {
+  const {
+    from,
+    to,
+    dispatch,
+    highSpeed,
+    departDate,
+    orderType,
+    searchParsed,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  } = props;
+  /* 接收form表单中的携带的数据并将数据初始化 */
+  useEffect(() => {
+    const queries = URI.parseQuery(window.location.search);
+    const { from, to, highSpeed, date } = queries;
+    dispatch(setFrom(from));
+    dispatch(setTo(to));
+    dispatch(setHighSpeed(highSpeed === "true"));
+    dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+    /* 将searchParsed改为true */
+    dispatch(setSearchParsed(true));
+  }, []);
+
+  /* 携带筛选条件从服务器中请求数据 */
+  useEffect(() => {
+    if (!searchParsed) {
+      return;
+    }
+
+    const url = new URI("/rest/query")
+      .setSearch("from", from)
+      .setSearch("to", to)
+      .setSearch("date", dayjs(departDate).format("YYYY-MM-DD"))
+      .setSearch("highSpeed", highSpeed)
+      .setSearch("orderType", orderType)
+      .setSearch("onlyTickets", onlyTickets)
+      .setSearch("checkedTicketTypes", Object.keys(checkedTicketTypes).join())
+      .setSearch("checkedTrainTypes", Object.keys(checkedTrainTypes).join())
+      .setSearch(
+        "checkedDepartStations",
+        Object.keys(checkedDepartStations).join()
+      )
+      .setSearch(
+        "checkedArriveStations",
+        Object.keys(checkedArriveStations).join()
+      )
+      .setSearch("departTimeStart", departTimeStart)
+      .setSearch("departTimeEnd", departTimeEnd)
+      .setSearch("arriveTimeStart", arriveTimeStart)
+      .setSearch("arriveTimeEnd", arriveTimeEnd)
+      .toString();
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        const {
+          dataMap: {
+            directTrainInfo: {
+              trains,
+              filter: { ticketType, trainType, depStation, arrStation },
+            },
+          },
+        } = result;
+
+        dispatch(setTrainList(trains));
+        dispatch(setTicketTypes(ticketType));
+        dispatch(setTrainTypes(trainType));
+        dispatch(setDepartStations(depStation));
+        dispatch(setArriveStations(arrStation));
+      });
+  }, [
+    from,
+    to,
+    departDate,
+    highSpeed,
+    searchParsed,
+    orderType,
+    onlyTickets,
+    checkedTicketTypes,
+    checkedTrainTypes,
+    checkedDepartStations,
+    checkedArriveStations,
+    departTimeStart,
+    departTimeEnd,
+    arriveTimeStart,
+    arriveTimeEnd,
+  ]);
+
+  const onBack = useCallback(() => {
+    window.history.back();
+  }, []);
   return (
     <div>
-        <Nav />
-        <List />
-        <Bottom  />
+      <div className="header-wrapper">
+        <Header title={`${from} => ${to}`} onBack={onBack} />
+      </div>
+      <Nav />
+      <List />
+      <Bottom />
     </div>
   );
 }
